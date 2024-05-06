92efa54cb30972197522b7ae15af63c8f1e13496
Coder.Colder <906254852@outlook.com>
2024-05-06 19:02:48 +0800

    feat(hooks): show unlimited dataview tasks for scheduling in timeline view

--- src/ui/hooks/use-visible-dataview-tasks.ts
+++ src/ui/hooks/use-visible-dataview-tasks.ts
@@ -1,5 +1,5 @@
 import { groupBy } from "lodash/fp";
-import { Moment } from "moment";
+import moment, { Moment } from "moment";
 import { STask } from "obsidian-dataview";
 import { derived, Readable } from "svelte/store";
 
@@ -18,26 +18,46 @@ export function useVisibleDataviewTasks(
     ([$visibleDays, $dataviewTasks, $settings]) => {
       const dayToSTasks = groupBy(getScheduledDay, $dataviewTasks);
 
-      return $visibleDays.reduce<Record<string, TasksForDay>>((result, day) => {
-        const key = getDayKey(day);
-        const sTasksForDay = dayToSTasks[key];
+      const result = $visibleDays.reduce<Record<string, TasksForDay>>(
+        (result, day) => {
+          const key = getDayKey(day);
+          const sTasksForDay = dayToSTasks[key];
 
-        if (sTasksForDay) {
-          // todo: process errors
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          const { errors, ...tasks } = mapToTasksForDay(
-            day,
-            sTasksForDay,
-            $settings,
-          );
+          if (sTasksForDay) {
+            // todo: process errors
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const { errors, ...tasks } = mapToTasksForDay(
+              day,
+              sTasksForDay,
+              $settings,
+            );
 
-          result[key] = tasks;
-        } else {
-          result[key] = getEmptyRecordsForDay();
-        }
+            result[key] = tasks;
+          } else {
+            result[key] = getEmptyRecordsForDay();
+          }
 
-        return result;
-      }, {});
+          return result;
+        },
+        {},
+      );
+
+      // show tasks without scheduled time
+      const day = moment();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { errors, ...tasks } = mapToTasksForDay(
+        day,
+        dayToSTasks["undefined"],
+        $settings,
+      );
+      const dayKey = getDayKey(day);
+      if (result[dayKey]) {
+        result[dayKey].noTime.push(...tasks.noTime);
+        result[dayKey].withTime.push(...tasks.withTime);
+      } else {
+        result[dayKey] = tasks;
+      }
+      return result;
     },
   );
 }
